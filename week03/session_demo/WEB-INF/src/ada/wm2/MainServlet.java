d0acbc4ae42dd1b06c7881b0a04bd4e5a16fd409
Leila Dostuyeva <99134123+leillards@users.noreply.github.com>
2022-03-28 12:28:52 +0400

    week3 submission by Leila

--- week03/session_demo/WEB-INF/src/ada/wm2/MainServlet.java
+++ week03/session_demo/WEB-INF/src/ada/wm2/MainServlet.java
@@ -1,8 +1,9 @@
 package ada.wm2;
 
-import javax.servlet.*;  
-import javax.servlet.http.*;
+import jakarta.servlet.*;  
+import jakarta.servlet.http.*;
 import java.util.*;
+import java.io.*;
 
 public class MainServlet extends HttpServlet{
 
@@ -16,7 +17,34 @@ public class MainServlet extends HttpServlet{
 		users.add("Sveta");
 	}
 
-	
+	public void doPost(HttpServletRequest req, HttpServletResponse res){
+		String username = req.getParameter("username");
+		String password = req.getParameter("password");
 
+
+		if("admin".equals(username) && "admin".equals(password)){
+		//successgull login
+		//forward to review
+			HttpSession session = req.getSession();
+			session.setAttribute("current_user", username);
+			session.setAttribute("user_list",users);
+			try{
+				RequestDispatcher disp = req.getRequestDispatcher("user.jsp");
+				disp.forward(req, res);
+			}catch(Exception ex){
+				System.out.println("Forward failed" + ex);
+			}
+			
+		}else{
+			//login failed
+			//redirect to login.html
+			//comment
+			try{
+				res.sendRedirect("index.html");
+			}catch(Exception ex){
+				System.out.println("Redirect failed" + ex);
+			}
+		}
+	}
 
 }
--- week03/session_demo/users.jsp
+++ week03/session_demo/users.jsp
@@ -6,6 +6,18 @@
 </head>
 <body>
 
+
+
+<%
+   List<String> users =(List)session.getAttribute("user_list");
+   String current_user =(String)session.getAttribute("current_user");
+   out.println("Welcome " + current_user);
+   for(String user : users){
+   	out.println(user);
+   }
+   //comment
+%>
+
 	
 </body>
 </html>
