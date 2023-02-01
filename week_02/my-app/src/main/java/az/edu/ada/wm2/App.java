9f36d18b3fbe9ece09e8914ef4085c5c14021bba
rafaelmamedov4312 <rfmammadov2021@ada.edu.az>
2023-02-01 20:21:13 +0400

    creating maven standalone java application

---
+++ week_02/my-app/src/main/java/az/edu/ada/wm2/App.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
