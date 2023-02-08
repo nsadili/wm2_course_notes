28076a72c90386e9df615164fc9de3d0d6a14447
ShamsiyyaAlakbarova <shamsnajafli@gmail.com>
2023-02-09 00:24:31 +0400

    spring boot

---
+++ week 03/first-spring-app/src/test/java/az/edu/ada/wm2/firstspringapp/FirstSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.firstspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
