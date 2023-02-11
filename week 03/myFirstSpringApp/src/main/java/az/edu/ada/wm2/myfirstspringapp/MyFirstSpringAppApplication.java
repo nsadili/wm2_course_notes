c4a8774eb986de4a2f3a8d03758295cdb64a36bf
Togrul Asadov <adsads00123@gmail.com>
2023-02-11 20:21:50 +0400

    bookapp created

--- week 03/myFirstSpringApp/src/main/java/az/edu/ada/wm2/myfirstspringapp/MyFirstSpringAppApplication.java
+++ week 03/myFirstSpringApp/src/main/java/az/edu/ada/wm2/myfirstspringapp/MyFirstSpringAppApplication.java
@@ -3,8 +3,12 @@ package az.edu.ada.wm2.myfirstspringapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MyFirstSpringAppApplication {
 
     public static void main(String[] args) {
@@ -16,8 +20,8 @@ public class MyFirstSpringAppApplication {
         return "Toghrul";
     }
 
-    @RequestMapping()
-    public String user() {
-
+    @RequestMapping("/api/users/{id}")
+    public String user(@PathVariable final long id) {
+        return "User with id " + id;
     }
 }
