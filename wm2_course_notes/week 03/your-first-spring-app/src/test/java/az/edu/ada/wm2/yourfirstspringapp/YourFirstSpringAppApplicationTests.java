f82faa5c2af5033af485515d2dc18068accc6c92
fahminmahili <118613088+fahminmahili@users.noreply.github.com>
2023-02-13 23:58:32 +0400

    Add files via upload

---
+++ wm2_course_notes/week 03/your-first-spring-app/src/test/java/az/edu/ada/wm2/yourfirstspringapp/YourFirstSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.yourfirstspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourFirstSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ wm2_course_notes/week 03/your-first-spring-app/your-first-spring-app/src/main/java/az/ada/edu/wm2/yourfirstspringapp/YourFirstSpringAppApplication.java
@@ -0,0 +1,13 @@
+package az.ada.edu.wm2.yourfirstspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourFirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourFirstSpringAppApplication.class, args);
+	}
+
+}
