73607a236fb84e405d89d0ae979b25c9df4a7e77
nijattt <78231857+NijatShv@users.noreply.github.com>
2023-02-14 09:29:58 +0400

    week031st

---
+++ week 03/nijats-first-spring-app/src/test/java/az/edu/ada/wm2/nijatsfirstspringapp/NijatsFirstSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.nijatsfirstspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NijatsFirstSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
