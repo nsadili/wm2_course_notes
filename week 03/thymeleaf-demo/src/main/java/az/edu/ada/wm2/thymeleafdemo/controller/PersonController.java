afdd24ebbe059db0435d63e43dd918d4d7a713cd
UlviyyaMammadova <umammadova7898@ada.edu.az>
2023-02-15 13:08:54 +0400

    test

--- week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/controller/PersonController.java
+++ week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/controller/PersonController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PersonController {
@@ -14,7 +15,15 @@ public class PersonController {
     @GetMapping({"/", "/list"})
     public String getAllPersons(Model model){
         model.addAttribute("personList",personService.list());
-        return "index"; //simple hdml file
+        return "index"; //simple html file
+    }
+    @GetMapping("/newPerson")
+    public ModelAndView showNewperson(){
+
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("new_person");
+
+        return mv;
     }
 
 }
