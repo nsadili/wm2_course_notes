138dcd37cf2d95cefc0389c3194f98ef4c88fbb1
Nuraddin Sadili <nsadili@ada.edu.az>
2023-02-15 14:03:05 +0400

    Complete thyemeleaf-demo with crud, image and css

--- week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/controller/PersonController.java
+++ week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/controller/PersonController.java
@@ -1,13 +1,53 @@
 package az.edu.ada.wm2.thymeleafdemo.controller;
 
+import az.edu.ada.wm2.thymeleafdemo.model.Person;
 import az.edu.ada.wm2.thymeleafdemo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PersonController {
     @Autowired
     private PersonService personService;
 
+    @GetMapping({"/", "/list"})
+    public String getAllPersons(Model model) {
+        model.addAttribute("personList", personService.list());
+
+        return "index";
+    }
+
+    @GetMapping("/newPerson")
+    public ModelAndView showNewPerson() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("new_person");
+
+        mv.addObject("person", new Person());
+        return mv;
+    }
+
+    @PostMapping("/save")
+    public String savePerson(@ModelAttribute Person person) {
+        personService.save(person);
+        return "redirect:/";
+    }
+
+    @GetMapping("/update/{id}")
+    public String showUpdatePerson(@PathVariable String id, Model model) {
+        Person person = personService.getById(id);
+        model.addAttribute("person", person);
+
+        return "update_person";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deletePerson(@PathVariable("id") String id){
+        personService.deleteById(id);
+
+        return "redirect:/";
+    }
 }
 
