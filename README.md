35e3e20e92a2789f7c3ff17e87b34e893f8da847
Elnur1111 <113518027+Elnur1111@users.noreply.github.com>
2023-02-16 12:45:21 +0400

    Week_4

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # wm2_course_notes
 
 <strong>DO NOT Update main branch.</strong>
-
-<em> I'am Elnur Aliyev...   </em>
---
+++ week 04/session-attributes-demo/src/main/java/az/edu/ada/wm2/springmvc/sessionattributesdemo/controller/OrderController.java
@@ -0,0 +1,33 @@
+package az.edu.ada.wm2.springmvc.sessionattributesdemo.controller;
+
+import az.edu.ada.wm2.springmvc.sessionattributesdemo.model.Order;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.DefaultSessionAttributeStore;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.context.request.WebRequest;
+
+
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+    @GetMapping("/")
+    public String listOrderDetailes(Model model, @SessionAttribute Order order){
+
+        model.addAttribute("order", order);
+        return "order/order_detail";
+
+    }
+
+    @PostMapping("/save")
+    public String saveOrder(@RequestParam String customerName, @SessionAttribute Order order){
+
+        order.setCustomerName(customerName);
+        System.out.println("Saving the order: " + order);
+        return "redirect:/products/list";
+
+
+    }
+}
