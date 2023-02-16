babdeb3b1b2e4194e32bf3b7f070c89c0a07366d
fahminmahili <118613088+fahminmahili@users.noreply.github.com>
2023-02-16 11:32:35 +0400

    new changes

---
+++ week 03/your-first-spring-app/your-first-spring-app/src/main/java/az/edu/ada/wm2/yourfirstspringapp/YourFirstSpringAppApplication.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.yourfirstspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourFirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourFirstSpringAppApplication.class, args);
+	}
+
+}
