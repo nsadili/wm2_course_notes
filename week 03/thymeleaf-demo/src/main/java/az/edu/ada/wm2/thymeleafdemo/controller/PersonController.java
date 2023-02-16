cca7a26e1f03f4bb478d471dd0f4c4cb922ffdf0
ShamsiyyaAlakbarova <shamsnajafli@gmail.com>
2023-02-16 11:11:53 +0400

    week3

--- week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/controller/PersonController.java
+++ week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/controller/PersonController.java
@@ -1,13 +1,23 @@
 package az.edu.ada.wm2.thymeleafdemo.controller;
-
+import az.edu.ada.wm2.thymeleafdemo.model.Person;
 import az.edu.ada.wm2.thymeleafdemo.service.PersonService;
+/*import javax.naming.directory.AttributeInUseException;*/
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PersonController {
     @Autowired
     private PersonService personService;
+
+    @GetMapping({"/", "list"})
+    public String getAllPersons(Model model){
+        model.addAllAttribute(attributeName "persons", personService.list());
+        return "index";
+
+    }
 
 }
 
