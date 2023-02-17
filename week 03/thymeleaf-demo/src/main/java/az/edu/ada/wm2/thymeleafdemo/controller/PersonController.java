949dced1b34bf0b713e649dd57b66a17f89e2652
UlviyyaMammadova <umammadova7898@ada.edu.az>
2023-02-17 10:55:21 +0400

    changes on week3

--- week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/controller/PersonController.java
+++ week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/controller/PersonController.java
@@ -1,10 +1,11 @@
 package az.edu.ada.wm2.thymeleafdemo.controller;
 
+import az.edu.ada.wm2.thymeleafdemo.model.Person;
 import az.edu.ada.wm2.thymeleafdemo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,17 +14,41 @@ public class PersonController {
     private PersonService personService;
 
     @GetMapping({"/", "/list"})
-    public String getAllPersons(Model model){
+    public String getAllPersons(Model model){ //another container carrying the data between controller and the new
         model.addAttribute("personList",personService.list());
         return "index"; //simple html file
     }
     @GetMapping("/newPerson")
-    public ModelAndView showNewperson(){
+    public ModelAndView showNewperson() {
 
         ModelAndView mv = new ModelAndView();
         mv.setViewName("new_person");
 
+        mv.addObject("person", new Person());
+
         return mv;
+    }
+    @PostMapping("/save")
+    public String savePerson(@ModelAttribute Person person){
+        personService.save(person);
+        return"redirect:/";
+    }
+
+    @GetMapping("/update/{id}")
+
+    public String showUpdatePerson(@PathVariable String id, Model model){
+        Person person = personService.getById(id);
+        model.addAttribute("person", person);
+
+        return "update_person";
+    }
+
+    @GetMapping("/delete/{id}")
+
+    public String deletePerson (@PathVariable("id") String id){
+        personService.deleteById(id);
+
+        return"redirect:/";
     }
 
 }
--- week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/model/Person.java
+++ week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/model/Person.java
@@ -1,26 +1,33 @@
 package az.edu.ada.wm2.thymeleafdemo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
 
     private String id;
     private String firstName;
     private String lastName;
 
-    public Person() {
-    }
+    /*public Person() {
+    }*/
 
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
-    public Person(String id, String firstName, String lastName) {
+    /*public Person(String id, String firstName, String lastName) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
-    }
+    }*/
 
-    public String getId() {
+   /*public String getId() {
         return id;
     }
 
@@ -51,5 +58,5 @@ public class Person {
                 ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
                 '}';
-    }
+    }*/
 }
