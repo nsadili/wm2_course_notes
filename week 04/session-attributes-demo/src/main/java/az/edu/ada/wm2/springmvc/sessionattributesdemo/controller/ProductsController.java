62fd5846f967352de923b0a3bd3c47ac32642044
rafaelmamedov4312 <rfmammadov2021@ada.edu.az>
2023-02-18 00:17:31 +0400

    working on session attribute demo

--- week 04/session-attributes-demo/src/main/java/az/edu/ada/wm2/springmvc/sessionattributesdemo/controller/ProductsController.java
+++ week 04/session-attributes-demo/src/main/java/az/edu/ada/wm2/springmvc/sessionattributesdemo/controller/ProductsController.java
@@ -4,8 +4,30 @@ import az.edu.ada.wm2.springmvc.sessionattributesdemo.model.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
+@Controller
+@RequestMapping("/products")
+@SessionAttributes("order")
 public class ProductsController {
+    @GetMapping({"/list","/"})
+    public String listProducts(Model model, @ModelAttribute Order order){
+        model.addAttribute("order",order== null?new Order():order);
 
 
+        return "products/list";
+    }
+
+    @PostMapping("addToOrder")
+    public String addProductToOrder(@RequestParam("product") String productId,
+                                    @ModelAttribute Order order) {
+       order.addProduct(productId);
+        return "redirect:/products/";
+
+
+    }
+
+    @ModelAttribute
+    public Order order(){
+
+        return new Order();
+    }
 }
