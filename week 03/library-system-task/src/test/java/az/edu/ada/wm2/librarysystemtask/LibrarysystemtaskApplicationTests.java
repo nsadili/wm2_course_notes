6dc7f96893ef1cc2f9e4e0b5e99f23ca80c386b7
rafaelmamedov4312 <rfmammadov2021@ada.edu.az>
2023-02-17 22:06:01 +0400

    library task is done

---
+++ week 03/library-system-task/src/test/java/az/edu/ada/wm2/librarysystemtask/LibrarysystemtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.librarysystemtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarysystemtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
