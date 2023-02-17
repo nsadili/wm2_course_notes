5f05903e5032e4d34398b4b7292f70f93e84bfff
rafaelmamedov4312 <rfmammadov2021@ada.edu.az>
2023-02-18 03:34:49 +0400

    working on shopping app task

---
+++ week 04/shopping-app-task/src/main/java/az/edu/ada/wm2/shoppingapptask/ShoppingAppTaskApplication.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.shoppingapptask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingAppTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingAppTaskApplication.class, args);
+	}
+
+}
---
+++ week 04/shopping-app-task/src/test/java/az/edu/ada/wm2/shoppingapptask/ShoppingAppTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.shoppingapptask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAppTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
