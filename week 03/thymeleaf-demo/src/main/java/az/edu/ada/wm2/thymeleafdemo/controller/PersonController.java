9efbff57e52ce189533acb8131c26853d1fe31d7
Laman Taghiyeva <lamantaghiyevaltaghiyeva@Lamans-MacBook-2.local>
2023-02-20 21:54:31 +0400

    Get Mapping

--- week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/controller/PersonController.java
+++ week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/controller/PersonController.java
@@ -1,13 +1,44 @@
 package az.edu.ada.wm2.thymeleafdemo.controller;
 
+import az.edu.ada.wm2.thymeleafdemo.model.Person;
 import az.edu.ada.wm2.thymeleafdemo.service.PersonService;
+import az.edu.ada.wm2.thymeleafdemo.service.PersonServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PersonController {
     @Autowired
     private PersonService personService;
 
+
+    @GetMapping({"/" , "list"})
+
+
+public String getAllPersons(Model model){
+
+model.addAttribute( "personList",personService.list());
+
+        return "index";
+}
+
+@GetMapping("/newPerson")
+    public ModelAndView showNewPersonPage(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("new_person");
+        mv.addObject("person", new Person());
+        return mv;
+}
+@PostMapping("/save")
+    public String save(@ModelAttribute Person person){
+        personService.save(person);
+
+        return "redirect:/";
+}
 }
 
