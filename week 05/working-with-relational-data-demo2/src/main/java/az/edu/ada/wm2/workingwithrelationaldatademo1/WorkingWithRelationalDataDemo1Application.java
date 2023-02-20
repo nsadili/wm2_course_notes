b275531b69c00dd235ad1d79282f17ce4a118de8
Nuraddin Sadili <nsadili@ada.edu.az>
2023-02-20 16:59:12 +0400

    Add week05_1 projects - working with data in a Spring Boot app using JDBC

---
+++ week 05/working-with-relational-data-demo2/src/main/java/az/edu/ada/wm2/workingwithrelationaldatademo1/WorkingWithRelationalDataDemo1Application.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.workingwithrelationaldatademo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingWithRelationalDataDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingWithRelationalDataDemo1Application.class, args);
+	}
+
+}
