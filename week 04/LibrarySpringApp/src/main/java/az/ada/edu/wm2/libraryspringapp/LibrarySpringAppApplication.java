a8622d73e8fb808b951c0e0ae451458171fcd1ae
1javid <jav.alakbarli@gmail.com>
2023-02-23 19:59:04 +0400

    added CRUD library app

---
+++ week 04/LibrarySpringApp/src/main/java/az/ada/edu/wm2/libraryspringapp/LibrarySpringAppApplication.java
@@ -0,0 +1,13 @@
+package az.ada.edu.wm2.libraryspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySpringAppApplication.class, args);
+	}
+
+}
