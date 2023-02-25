c49fd1162f37c5410b0c1dd84c4d99a904f83901
Togrul Asadov <adsads00123@gmail.com>
2023-02-26 02:15:27 +0400

    course app added (week5 task)

--- lmsapp/src/main/java/az/edu/ada/wm2/lmsapp/entity/Course.java
+++ lmsapp/src/main/java/az/edu/ada/wm2/lmsapp/entity/Course.java
@@ -1,6 +1,13 @@
 package az.edu.ada.wm2.lmsapp.entity;
 
+<<<<<<< HEAD
 import jakarta.persistence.*;
+=======
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+>>>>>>> 5f6b519 (course app added (week5 task))
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -11,7 +18,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Course {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String departmentName;
--- lmsapp/src/main/java/az/edu/ada/wm2/lmsapp/service/CourseService.java
+++ lmsapp/src/main/java/az/edu/ada/wm2/lmsapp/service/CourseService.java
@@ -2,40 +2,18 @@ package az.edu.ada.wm2.lmsapp.service;
 
 import az.edu.ada.wm2.lmsapp.entity.Course;
 import az.edu.ada.wm2.lmsapp.repository.CourseRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-@RequiredArgsConstructor
 public class CourseService {
-    private final CourseRepository courseRepository;
+    @Autowired
+    CourseRepository courseRepository;
 
-    public Iterable<Course> listCourses() {
-        return courseRepository.findAll();
-    }
-
-    public Page<Course> listCourses(int page) {
-        return courseRepository.findAll(PageRequest.of(page - 1, 10));
-    }
-
-    public Page<Course> listCourses(int page, int pageSize) {
-        return courseRepository.findAll(PageRequest.of(page - 1, pageSize));
-    }
-
-    public Page<Course> listCourses(int page, int pageSize, String sortBy) {
-        Sort sort;
-
-        if (sortBy == null)
-            return courseRepository.findAll(PageRequest.of(page - 1, pageSize));
-
-        System.out.println(sortBy);
-        sort = sortBy.charAt(0) == '-' ? Sort.by(sortBy.substring(1)).descending() : Sort.by(sortBy);
-        System.out.println(sort);
-
-        return courseRepository.findAll(PageRequest.of(page - 1, pageSize, sort));
+    public List<Course> listCourses() {
+        return (List<Course>) courseRepository.findAll();
     }
 
     public Course getById(Integer id) {
