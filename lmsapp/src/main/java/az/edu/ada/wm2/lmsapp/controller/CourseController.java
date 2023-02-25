8f33ad9cba9bb1223d30f3809a310b9f475ccff1
Togrul Asadov <adsads00123@gmail.com>
2023-02-26 02:15:27 +0400

    course app added (week5 task)

--- lmsapp/src/main/java/az/edu/ada/wm2/lmsapp/controller/CourseController.java
+++ lmsapp/src/main/java/az/edu/ada/wm2/lmsapp/controller/CourseController.java
@@ -2,48 +2,25 @@ package az.edu.ada.wm2.lmsapp.controller;
 
 import az.edu.ada.wm2.lmsapp.entity.Course;
 import az.edu.ada.wm2.lmsapp.service.CourseService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/courses")
-@RequiredArgsConstructor
 public class CourseController {
-    private final CourseService courseService;
+    @Autowired
+    CourseService courseService;
 
-    @GetMapping(value = {"", "/list"})
+    @GetMapping({"", "/list"})
     public String listCourses(Model model) {
         model.addAttribute("courses", courseService.listCourses());
         return "course/list";
     }
 
-    @GetMapping(value = {"", "/list"}, params = {"page"})
-    public String listCourses(Model model, @RequestParam Integer page) {
-        model.addAttribute("courses", courseService.listCourses(page));
-        return "course/list";
-    }
-
-    @GetMapping(value = {"", "/list"}, params = {"page", "page-size"})
-    public String listCourses(Model model,
-                              @RequestParam Integer page,
-                              @RequestParam(name = "page-size") Integer pageSize) {
-        model.addAttribute("courses", courseService.listCourses(page, pageSize));
-        return "course/list";
-    }
-
-    @GetMapping(value = {"", "/list"}, params = {"page", "page-size", "sort"})
-    public String listCourses(Model model,
-                              @RequestParam Integer page,
-                              @RequestParam(name = "page-size") Integer pageSize,
-                              @RequestParam(name = "sort", required = false) String sortBy) {
-        model.addAttribute("courses", courseService.listCourses(page, pageSize, sortBy));
-        return "course/list";
-    }
-
     @GetMapping("/{id}")
-    public String getCourse(Model model, @PathVariable Integer id) {
+    public String listCourses(Model model, @PathVariable Integer id) {
         model.addAttribute("course", courseService.getById(id));
         return "course/detail";
     }
--- lmsapp/src/main/java/az/edu/ada/wm2/lmsapp/entity/Course.java
+++ lmsapp/src/main/java/az/edu/ada/wm2/lmsapp/entity/Course.java
@@ -1,6 +1,9 @@
 package az.edu.ada.wm2.lmsapp.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -11,7 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Course {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String departmentName;
