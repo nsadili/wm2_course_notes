82e5c8b875d063ebbf6f68b53beabba9ea9ccb60
Togrul Asadov <adsads00123@gmail.com>
2023-02-26 02:15:27 +0400

    course app added (week5 task)

--- lmsapp/src/main/java/az/edu/ada/wm2/lmsapp/repository/CourseRepository.java
+++ lmsapp/src/main/java/az/edu/ada/wm2/lmsapp/repository/CourseRepository.java
@@ -2,9 +2,6 @@ package az.edu.ada.wm2.lmsapp.repository;
 
 import az.edu.ada.wm2.lmsapp.entity.Course;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CourseRepository extends
-        CrudRepository<Course, Integer>,
-        PagingAndSortingRepository<Course, Integer> {
+public interface CourseRepository extends CrudRepository<Course, Integer> {
 }
