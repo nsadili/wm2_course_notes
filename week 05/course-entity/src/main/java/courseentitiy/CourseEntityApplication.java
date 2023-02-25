4580cc0e0ac838dfe47031cc89492d071876dc64
Laman Taghiyeva <lamantaghiyevaltaghiyeva@Lamans-MacBook-2.local>
2023-02-26 02:00:08 +0400

    Library System

---
+++ week 05/course-entity/src/main/java/courseentitiy/CourseEntityApplication.java
@@ -0,0 +1,13 @@
+package courseentitiy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseEntityApplication.class, args);
+	}
+
+}
---
+++ week 05/course-entity/src/test/java/courseentitiy/CourseEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package courseentitiy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
