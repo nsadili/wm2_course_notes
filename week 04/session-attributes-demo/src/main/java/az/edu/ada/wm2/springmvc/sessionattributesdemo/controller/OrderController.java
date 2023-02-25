caa0068d051c07dfc018472dec41760d18ee5df7
SenanM <95751921+SenanM1@users.noreply.github.com>
2023-02-25 23:57:11 +0400

    session-attributes-demo Done.

--- week 04/session-attributes-demo/src/main/java/az/edu/ada/wm2/springmvc/sessionattributesdemo/controller/OrderController.java
+++ week 04/session-attributes-demo/src/main/java/az/edu/ada/wm2/springmvc/sessionattributesdemo/controller/OrderController.java
@@ -8,7 +8,20 @@ import org.springframework.web.bind.support.DefaultSessionAttributeStore;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.context.request.WebRequest;
 
+@Controller
+@RequestMapping("/orders")
 public class OrderController {
 
+    @GetMapping("/")
+    public String listOrdersDetails(Model model, @SessionAttribute Order order){
+        model.addAttribute("order", order);
+        return "orders/order_detail";
+    }
 
+    @PostMapping("/save")
+    public String saveOrder(@RequestParam String customerName, @SessionAttribute Order order){
+        order.setCustomerName(customerName);
+        System.out.println("Saving the order: " + order);
+        return "redirect:/products/list";
+    }
 }
--- week 04/session-attributes-demo/src/main/java/az/edu/ada/wm2/springmvc/sessionattributesdemo/controller/ProductsController.java
+++ week 04/session-attributes-demo/src/main/java/az/edu/ada/wm2/springmvc/sessionattributesdemo/controller/ProductsController.java
@@ -5,7 +5,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+@Controller
+@RequestMapping("/products")
+@SessionAttributes({"order"})
 public class ProductsController {
 
+    @GetMapping({"list","/"})
+    public String listProducts(Model model, @ModelAttribute Order order){
+        model.addAttribute("order", order == null ? new Order() : order);
+        return "products/list";
+    }
 
+    @PostMapping("addToOrder")
+    public String addProductToOrder(@RequestParam("product") String productId,
+                                    @ModelAttribute Order order,
+                                    Model model){
+        if (order == null) order = new Order();
+        if (productId != null && !productId.isBlank()) order.addProduct(productId);
+        model.addAttribute("order",order);
+        return "redirect:/products/list";
+    }
+
+    @ModelAttribute
+    public Order order() {
+        return new Order();
+    }
 }
