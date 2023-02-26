b245cefc9b08286a75707e89068995787ceabfb7
Laman Taghiyeva <lamantaghiyevaltaghiyeva@Lamans-MacBook-2.local>
2023-02-26 13:54:29 +0400

    Book Controller

---
+++ week 05/Data-JDBC-connectivity/src/main/java/com/example/librarysystem/controller/BookController.java
@@ -0,0 +1,48 @@
+package com.example.librarysystem.controller;
+
+import com.example.librarysystem.model.Book;
+import com.example.librarysystem.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book createdBook = bookService.createBook(book);
+        return new ResponseEntity<>(createdBook, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> readBook(@PathVariable Long id) {
+        Book book = bookService.readBook(id);
+        return new ResponseEntity<>(book, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id, @RequestBody Book book) {
+        Book updatedBook = bookService.updateBook(id, book);
+        return new ResponseEntity<>(updatedBook, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+
+}
--- week 05/Data-JDBC-connectivity/src/main/java/com/example/librarysystem/service/BookService.java
+++ week 05/Data-JDBC-connectivity/src/main/java/com/example/librarysystem/service/BookService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 import java.util.List;
-@Service 
+@Service
 public class BookService {
 
     private final BookRepository bookRepository;
