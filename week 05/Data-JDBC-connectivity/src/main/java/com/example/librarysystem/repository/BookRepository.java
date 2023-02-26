45fd90c09f20c7e0d5fa27fbab0937d13c4ba054
Laman Taghiyeva <lamantaghiyevaltaghiyeva@Lamans-MacBook-2.local>
2023-02-26 13:37:23 +0400

    Book Service Class created

---
+++ week 05/Data-JDBC-connectivity/src/main/java/com/example/librarysystem/repository/BookRepository.java
@@ -0,0 +1,6 @@
+package com.example.librarysystem.repository;
+import com.example.librarysystem.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
---
+++ week 05/Data-JDBC-connectivity/src/main/java/com/example/librarysystem/service/BookService.java
@@ -0,0 +1,56 @@
+package com.example.librarysystem.service;
+
+
+import com.example.librarysystem.model.Book;
+import com.example.librarysystem.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import java.util.List;
+@Service 
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public Book createBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public List<Book> readAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book readBook(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public Book updateBook(Long id, Book book) {
+        Book existingBook = bookRepository.findById(id).orElse(null);
+        if (existingBook != null) {
+            existingBook.setTitle(book.getTitle());
+            existingBook.setAuthor(book.getAuthor());
+            existingBook.setPublisher(book.getPublisher());
+            existingBook.setIsbn(book.getIsbn());
+            existingBook.setQuantity(book.getQuantity());
+            existingBook.setPrice(book.getPrice());
+            return bookRepository.save(existingBook);
+        }
+        return null;
+    }
+
+    public boolean deleteBook(Long id) {
+        Book book = bookRepository.findById(id).orElse(null);
+        if (book != null) {
+            bookRepository.delete(book);
+            return true;
+        }
+        return false;
+    }
+}
