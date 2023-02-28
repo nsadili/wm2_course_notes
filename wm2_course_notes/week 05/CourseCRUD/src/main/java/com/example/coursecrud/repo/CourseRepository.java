d3f88a7d6bc40ed8eda3031063f1c4e5eb5295d2
Abdullah Kazimov <89588544+abdkzmv@users.noreply.github.com>
2023-02-28 10:00:03 +0400

    Delete wm2_course_notes/week 05/CourseCRUD directory

--- wm2_course_notes/week 05/CourseCRUD/src/main/java/com/example/coursecrud/repo/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.coursecrud.repo;
-
-import com.example.coursecrud.model.Course;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CourseRepository extends CrudRepository<Course, Integer> {
-}
