678c34a247217a23d4be499c7b83cec910cf00e8
Abdullah Kazimov <89588544+abdkzmv@users.noreply.github.com>
2023-02-28 09:59:41 +0400

    Course Management CRUD App

---
+++ week 05/CourseCRUD/src/main/java/com/example/coursecrud/CourseCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseCrudApplication.class, args);
+    }
+
+}
---
+++ week 05/CourseCRUD/src/test/java/com/example/coursecrud/CourseCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coursecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
