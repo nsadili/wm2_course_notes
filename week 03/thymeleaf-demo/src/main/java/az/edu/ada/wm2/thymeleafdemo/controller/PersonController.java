d04b1964d90220b2506837a3aa20b2df602c4025
sabinaveyisli <115728318+sabinaveyisli@users.noreply.github.com>
2023-03-01 12:21:51 +0400

    New Update

--- week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/controller/PersonController.java
+++ week 03/thymeleaf-demo/src/main/java/az/edu/ada/wm2/thymeleafdemo/controller/PersonController.java
@@ -3,11 +3,20 @@ package az.edu.ada.wm2.thymeleafdemo.controller;
 import az.edu.ada.wm2.thymeleafdemo.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PersonController {
     @Autowired
     private PersonService personService;
+
+    @GetMapping({"/", "list"})
+    public String getAllPersons(Model model){
+        model.addAttribute("personList", personService.list());
+
+        return "index";
+    }
 
 }
 
