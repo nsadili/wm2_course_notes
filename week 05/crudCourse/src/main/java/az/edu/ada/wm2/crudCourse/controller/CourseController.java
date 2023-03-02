faed0896f2d6df73db9a95692ec28be04094a88f
Minura Hajisoy <minura.hajisoy@gmail.com>
2023-03-02 11:26:17 +0400

    crud operation

--- week 05/crudCourse/src/main/java/az/edu/ada/wm2/crudCourse/controller/CourseController.java
+++ week 05/crudCourse/src/main/java/az/edu/ada/wm2/crudCourse/controller/CourseController.java
@@ -3,32 +3,51 @@ package az.edu.ada.wm2.crudCourse.controller;
 import az.edu.ada.wm2.crudCourse.entity.Course;
 import az.edu.ada.wm2.crudCourse.service.CourseService;
 import az.edu.ada.wm2.crudCourse.service.impl.CourseServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/courses")
 public class CourseController {
-
-    @GetMapping("/list")
-    public String getBooks(Model model) {
-        model.addAttribute("course");
-
-        List<Course> courses = new CourseServiceImpl().list();
-
-        model.addAttribute("courses", courses;
-        return "course_list";
+    @Autowired
+    private CourseService courseService;
+    @GetMapping({"/","/list"})
+    public String getAllCourses(Model model){
+        model.addAttribute("courses", courseService.list());
+        return "index";
     }
 
-    @GetMapping("/{id}")
-    public String showUserForm(@PathVariable("id") Course course,
-                               Model model) {
-        model.addAttribute("course", course);
-        return "user_form";
+    @GetMapping("/newCourse")
+    public ModelAndView showNewCoursePage(){
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("new_course");
+        mv.addObject("course", new Course());
+        return mv;
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute Course course){
+        courseService.save(course);
+        return "redirect:/courses/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteCourse(@PathVariable String id) {
+        courseService.deleteById(Integer.valueOf(id));
+        return "redirect:/courses/";
+    }
+    @GetMapping("/update/{id}")
+    public ModelAndView updateCourse(@PathVariable String id) {
+        ModelAndView mv = new ModelAndView("update_course");
+        // mv.setViewName("update_person");
+        mv.addObject("course", courseService.getById(Integer.valueOf(id)));
+        return mv;
     }
 }
