122058909eee698a9ad542ff83da4b307e9c0da5
UlviyyaMammadova <umammadova7898@ada.edu.az>
2023-03-02 21:02:22 +0400

    Some updates

--- week 05/course/course/src/main/java/az/edu/ada/wm2/course/controller/CourseController.java
+++ week 05/course/course/src/main/java/az/edu/ada/wm2/course/controller/CourseController.java
@@ -1,6 +1,8 @@
 package az.edu.ada.wm2.course.controller;
 
 import az.edu.ada.wm2.course.entity.Course;
+import az.edu.ada.wm2.course.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class CourseController {
+    @Autowired
+    private CourseService courseService;
     @GetMapping("/{id}")
     public String showUserForm(@PathVariable("id")Course course,
                                Model model){
