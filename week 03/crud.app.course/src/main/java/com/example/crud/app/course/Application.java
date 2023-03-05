281ad2c0107785166a10a48e5238c7896ff5b985
nijattt <78231857+NijatShv@users.noreply.github.com>
2023-03-05 14:36:43 +0400

    Added new files to repository

---
+++ week 03/crud.app.course/src/main/java/com/example/crud/app/course/Application.java
@@ -0,0 +1,17 @@
+package com.example.crud.app.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+
+
