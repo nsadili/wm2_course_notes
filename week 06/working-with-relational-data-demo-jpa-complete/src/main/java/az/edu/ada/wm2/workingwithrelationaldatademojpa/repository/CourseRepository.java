fee93d0b3e7a11ecc18960dcdb4d7490552c818b
Nuraddin Sadili <nsadili@ada.edu.az>
2023-03-06 14:59:26 +0400

    Add week 06 Complete JPA project including custom queries and pagination

---
+++ week 06/working-with-relational-data-demo-jpa-complete/src/main/java/az/edu/ada/wm2/workingwithrelationaldatademojpa/repository/CourseRepository.java
@@ -0,0 +1,26 @@
+package az.edu.ada.wm2.workingwithrelationaldatademojpa.repository;
+
+import az.edu.ada.wm2.workingwithrelationaldatademojpa.model.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.awt.print.Pageable;
+
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+//    Page<Course> findAll(Pageable pageable);
+
+    Iterable<Course> findByStudentsId(Long id);
+
+    @Query("select c from Course c where c not in " +
+            "(select c from Course c left join c.students std where std.id = :id)")
+    Iterable<Course> findByStudentsIdNot(Long id);
+
+    @Query("select c from Course c where lower(c.description) like %:keyword%")
+    Iterable<Course> getAllWebCoursesUsingJPAQuery(@Param("keyword") String keyword);
+
+    @Query(value = "select * from courses where description like '%Web%'", nativeQuery = true)
+    Iterable<Course> getAllWebCoursesUsingNativeQuery();
+}
