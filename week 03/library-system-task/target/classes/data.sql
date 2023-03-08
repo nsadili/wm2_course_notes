da385f171484e37d26124c0bf6c339e1c98dca8e
rafaelmamedov4312 <rfmammadov2021@ada.edu.az>
2023-03-08 19:49:32 +0400

    fixin isue

---
+++ week 03/library-system-task/target/classes/data.sql
@@ -0,0 +1,6 @@
+DELETE from library;
+
+INSERT INTO library(title, description,yearr,author) values
+                                              ('Nuraddin', 'Sadili','ads','ali'),
+                                              ('Jamal', 'Ismayilzada','sdf','pirveli'),
+                                              ('Ali', 'Mammadov','ili','agali');
---
+++ week 03/library-system-task/target/classes/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `library` (
+                                         `id` INT NOT NULL AUTO_INCREMENT,
+                                         `title` VARCHAR(45) NOT NULL,
+                                         `description` VARCHAR(45) NOT NULL,
+                                         `yearr` VARCHAR(45) NOT NULL,
+                                         `author` VARCHAR(45) NOT NULL,
+                                         PRIMARY KEY (`id`)
+)
