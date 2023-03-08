65f88d685c6131936cc9cc6cc296ff49c07f8b1d
rafaelmamedov4312 <rfmammadov2021@ada.edu.az>
2023-03-08 19:57:01 +0400

    changes

--- week 03/your-first-spring-app/src/main/java/az/edu/ada/wm2/yourfirstspringapp/YourFirstSpringAppApplication.java
+++ week 03/your-first-spring-app/src/main/java/az/edu/ada/wm2/yourfirstspringapp/YourFirstSpringAppApplication.java
@@ -38,19 +38,13 @@ public class YourFirstSpringAppApplication {
 
 
 @SpringBootApplication
-
 public class YourFirstSpringAppApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(YourFirstSpringAppApplication.class, args);
 
 		System.out.println("Printing your HELLO WORLD!");
-
 	}
 
 
-
 }
-
-
