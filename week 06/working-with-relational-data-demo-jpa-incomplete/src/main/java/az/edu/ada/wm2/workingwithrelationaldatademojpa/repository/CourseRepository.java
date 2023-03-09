3094e92ea258635f1920a1b0002508f3d1d3fe80
SenanM <95751921+SenanM1@users.noreply.github.com>
2023-03-09 23:45:17 +0400

    CourseRepositary Update

--- week 06/working-with-relational-data-demo-jpa-incomplete/src/main/java/az/edu/ada/wm2/workingwithrelationaldatademojpa/repository/CourseRepository.java
+++ week 06/working-with-relational-data-demo-jpa-incomplete/src/main/java/az/edu/ada/wm2/workingwithrelationaldatademojpa/repository/CourseRepository.java
@@ -4,5 +4,5 @@ import az.edu.ada.wm2.workingwithrelationaldatademojpa.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<Course, Long> {
-
+    
 }
