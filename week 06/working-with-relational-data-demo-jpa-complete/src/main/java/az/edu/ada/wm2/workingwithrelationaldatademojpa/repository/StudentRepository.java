3af7ebb6fbede7275b645e312030c133e10b7ac2
SenanM <95751921+SenanM1@users.noreply.github.com>
2023-03-24 02:04:58 +0400

    90% Done

--- week 06/working-with-relational-data-demo-jpa-complete/src/main/java/az/edu/ada/wm2/workingwithrelationaldatademojpa/repository/StudentRepository.java
+++ week 06/working-with-relational-data-demo-jpa-complete/src/main/java/az/edu/ada/wm2/workingwithrelationaldatademojpa/repository/StudentRepository.java
@@ -4,10 +4,12 @@ import az.edu.ada.wm2.workingwithrelationaldatademojpa.model.Student;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
     Page<Student> findAll(Pageable pageable);
+
 
     Iterable<Student> findByFirstNameAndLastName(String firstName, String lastName);
 
