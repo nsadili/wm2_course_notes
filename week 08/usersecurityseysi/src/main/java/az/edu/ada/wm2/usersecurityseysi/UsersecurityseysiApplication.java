f0256c128d5cc9551f9d32166ff6e0b42c991128
Minura Hajisoy <minura.hajisoy@gmail.com>
2023-04-06 11:37:23 +0400

    adding  codes

---
+++ week 08/usersecurityseysi/src/main/java/az/edu/ada/wm2/usersecurityseysi/UsersecurityseysiApplication.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.usersecurityseysi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersecurityseysiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersecurityseysiApplication.class, args);
+	}
+
+}
---
+++ week 08/usersecurityseysi/src/test/java/az/edu/ada/wm2/usersecurityseysi/UsersecurityseysiApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.usersecurityseysi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersecurityseysiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
