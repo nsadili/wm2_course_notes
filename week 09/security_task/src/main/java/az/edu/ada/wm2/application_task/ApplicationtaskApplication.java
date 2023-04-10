0df52bd6058fb778991ed7590611807f69b5aa2a
rafaelmamedov4312 <rfmammadov2021@ada.edu.az>
2023-04-10 17:47:54 +0400

    some taskes done

---
+++ week 09/security_task/src/main/java/az/edu/ada/wm2/application_task/ApplicationtaskApplication.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.application_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationtaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationtaskApplication.class, args);
+	}
+
+}
---
+++ week 09/security_task/src/test/java/az/edu/ada/wm2/application_task/ApplicationtaskApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.application_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationtaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
