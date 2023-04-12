ba0a21e785e236bd81ba0ad45a1361dca76db8fe
hvaliyev916 <huseyn_v02@outlook.com>
2023-04-12 16:17:24 +0400

    CRUD created but haven't started to work yet

---
+++ week 03/Crud-App/src/main/java/com/example/Crud/App/CrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Crud.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAppApplication.class, args);
+	}
+
+}
