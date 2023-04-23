e4a1e830eebfe4ee79ea17b30fbc2387ec1a7159
ShamsiyyaAlakbarova <93210034+ShamsiyyaAlakbarova@users.noreply.github.com>
2023-04-23 21:07:49 +0400

    Add files via upload

---
+++ week 10/spring-boot-security-framework-demo/src/main/java/az/edu/ada/wm2/springbootsecurityframeworkdemo/controller/WelcomeController.java
@@ -0,0 +1,34 @@
+package az.edu.ada.wm2.springbootsecurityframeworkdemo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @Autowired
+    @Qualifier("welcomeMessage")
+    private String welcomeText;
+
+    @Autowired
+    @Qualifier("farewellMessage")
+    private String farewellText;
+
+
+    @GetMapping("/")
+    public String welcomePage(Model model){
+        model.addAttribute("message", welcomeText);
+        return "welcome";
+    }
+
+    @GetMapping("/bye")
+    public String farewellPage(Model model){
+        model.addAttribute("message", farewellText);
+        return "welcome";
+    }
+
+
+}
