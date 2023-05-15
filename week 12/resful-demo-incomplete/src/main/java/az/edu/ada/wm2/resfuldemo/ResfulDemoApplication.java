b3736236e4ea35676e544b0a12146eb23e8226af
hvaliyev916 <huseyn_v02@outlook.com>
2023-05-15 10:27:10 +0400

    Week 11, dependency corrected;
    Other classes also pushed

---
+++ week 12/resful-demo-incomplete/src/main/java/az/edu/ada/wm2/resfuldemo/ResfulDemoApplication.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.resfuldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResfulDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResfulDemoApplication.class, args);
+	}
+
+}
---
+++ week 13/restful-consumer-demo/restful-consumer-demo/src/main/java/az/edu/ada/wm2/restfulconsumerdemo/RestfulConsumerDemoApplication.java
@@ -0,0 +1,13 @@
+package az.edu.ada.wm2.restfulconsumerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulConsumerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulConsumerDemoApplication.class, args);
+	}
+
+}
